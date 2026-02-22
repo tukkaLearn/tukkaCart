1d1c6b32f6f162983ccce7baecb0469f48dcb3d5
itzDm <bitdev20@gmail.com>
2026-02-22 10:38:33 +0530

    cart persist

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, type ReactNode } from "react";
+import { createContext, useEffect, useReducer, type ReactNode } from "react";
 import { cartReducer } from "../features/cart/cartReducer";
 import type { CartItem, CartAction } from "../features/cart/cartActions";
 
@@ -10,8 +10,14 @@ type CartContextType = {
 const CartContext = createContext<CartContextType | null>(null);
 
 export function CartProvider({ children }: { children: ReactNode }) {
-  const [cart, dispatch] = useReducer(cartReducer, []);
+  const [cart, dispatch] = useReducer(cartReducer, [], () => {
+    const storedCart = localStorage.getItem("cart");
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
 
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
   return (
     <CartContext.Provider value={{ cart, dispatch }}>
       {children}
